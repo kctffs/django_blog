a05fcb65c93b89fb72fa01704602afeb7b1a799d
Kieran Cattermole <kctffs@gmail.com>
2024-08-21 12:00:39 +0000

    readies code for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a8_-&_qe19x0o*n+e&x81ilpxt+9(&=cmdr!u6axt9@gp&^qbm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-kctffs-djangoblog-wifuu76tpwd.ws-eu115.gitpod.io']
+ALLOWED_HOSTS = ['8000-kctffs-djangoblog-wifuu76tpwd.ws-eu115.gitpod.io',
+                '.herokuapp.com']
 
 
 # Application definition
